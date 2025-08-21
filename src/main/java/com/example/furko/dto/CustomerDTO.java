d7b398937b94a60b2ae714deb5d4f16fb773667e
FurkanK98 <furkansanchez@outlook.de>
2025-08-22 00:35:14 +0200

    Added Swagger-Annotation (@Schema) to improve API model documentation in Swagger UI

--- src/main/java/com/example/furko/dto/CustomerDTO.java
+++ src/main/java/com/example/furko/dto/CustomerDTO.java
@@ -1,20 +1,29 @@
 package com.example.furko.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data // Generiert automatisch Getter, Setter, toString, equals und hashCode Methoden für alle Felder der Klasse
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDTO {
     @NotBlank(message = "Name darf nicht leer sein")
+    @Schema(example = "Furkan Test")
     private String name;
 
     @Email(message = "E-Mail muss gültig sein")
+    @Schema(example = "furkan@test.de")
     private String email;
 
     @Size(min = 10, max = 15, message = "Telefonnummer muss zwischen 10 und 15 Zeichen sein")
+    @Schema(example = "+49 123 456789")
     private String phoneNumber;
 
+    @Schema(example = "Furkanstr. 17, 12345 Teststadt")
     private String address;
 }
