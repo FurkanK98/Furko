103bd3ecdd5326c25ae13b0ee8862a21c394d89b
FurkanK98 <furkansanchez@outlook.de>
2025-08-26 23:58:46 +0200

    Set up API documentation to use token-based authentication for secure access.

--- src/main/java/com/example/furko/config/SwaggerConfig.java
+++ src/main/java/com/example/furko/config/SwaggerConfig.java
@@ -2,8 +2,11 @@
 
 package com.example.furko.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +15,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth")) // Authorize-Button oben rechts (Einloggen)
                 .info(new Info()
                         .title("Furko - Invoice Management API")
                         .version("1.0")
